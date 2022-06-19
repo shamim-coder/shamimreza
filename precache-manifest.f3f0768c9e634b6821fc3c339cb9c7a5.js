8ad321d78d26d88b1bc16e7c97cc2eaac76242cf
Shamim Reza <wpshamim@gmail.com>
2022-06-20 02:03:28 +0600

    Updates

---
+++ precache-manifest.f3f0768c9e634b6821fc3c339cb9c7a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77ca3d34e1d215016100141afa39bbe2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3ee1048598a7921e3ea",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "9528b34dacfc42fbac9f",
+    "url": "/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "f3ee1048598a7921e3ea",
+    "url": "/static/js/2.689f2f8e.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.689f2f8e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9528b34dacfc42fbac9f",
+    "url": "/static/js/main.66704534.chunk.js"
+  },
+  {
+    "revision": "b503dae912c004078b2e",
+    "url": "/static/js/runtime-main.df535f50.js"
+  }
+]);
